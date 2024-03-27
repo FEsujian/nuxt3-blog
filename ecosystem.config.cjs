7657b9d84281e2bfafa50f09ce386f2e98a5bf19
sujian <744929434@qq.com>
2024-03-27 11:53:45 +0800

    feat: 修改启动端口号

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'FEsujian',
-        port: '3001',
+        port: '8080',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'
