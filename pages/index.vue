7a137099e3643ac878af06fd7b09a8041171f5b7
sujian <744929434@qq.com>
2024-03-27 12:28:10 +0800

    feat: 修改启动配置

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,8 @@
 <template>
-  <!-- <div>
-      <h1>Color mode: {{ $colorMode.value }}</h1>
-      <select v-model="$colorMode.preference">
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="sepia">Sepia</option>
-      </select>
-    </div> -->
+  <div class="text-4xl font-bold text-center my-20">Sujian's Blog</div>
 </template>
 
 <script setup>
 </script>
 
-<style>
-</style>
+<style></style>
